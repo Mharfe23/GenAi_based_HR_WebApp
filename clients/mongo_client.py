b11135d7dad49212b175193849fa408237eb68d5
Mharfe23 <fengiro.arafa@gmail.com>
2025-07-23 21:41:08 +0100

    refactored mongo_init to use a singleton + added technologies mongo dictionary

--- clients/mongo_client.py
+++ clients/mongo_client.py
@@ -26,13 +26,52 @@ def _mongo_init(db_name="ResumeDB", collection_name="Candidats"):
         mongo_client = MongoClient(uri_mongo, server_api=ServerApi('1'))
         db = mongo_client[db_name]
         collection = db[collection_name]
-        logger.info("MongoDB connection established successfully.")
+        logger.info(f"MongoDB connection established successfully for collection: {collection_name}.")
         return collection
     except PyMongoError as e:
         logger.error(f"MongoDB connection failed: {e}")
         raise
 
-collection = _mongo_init()
+##For Singleton DB connection
+collection_candidat = _mongo_init()
 
-def mongo_init():
-    return collection
+
+def mongo_candidat_init():
+    return collection_candidat
+
+################# Dictionnary (Set in this case) for skills 
+
+##For Singleton DB connection
+collection_skills = _mongo_init(collection_name="skills")
+
+def add_new_technologies(new_tech, doc_id="tech_stack"):
+    """Add new technologies to existing ones """
+    collection_skills.update_one(
+        {"_id":doc_id},
+        {
+            "$addToSet":{
+                "technologies":{"$each": new_tech} 
+                }
+        }
+    )
+
+def replace_all_technologies(tech_list, doc_id="tech_stack"):
+    """ Replace the whole technologies list"""
+
+    collection_skills.update_one(
+        {"_id": "tech_stack"},
+        {"$set": {"technologies": tech_list}}
+    )
+
+
+def init_techs_if_not_exist(tech_list, doc_id="tech_stack"):
+    """Init the first technologies list"""
+    existing_doc = collection_skills.find_one({"_id": "tech_stack"})
+    if not existing_doc:
+        collection_skills.insert_one({
+            "_id": "tech_stack",
+            "technologies": tech_list
+        })
+        logger.info("Inserted new tech stack document.")
+    else:
+        logger.info("Document already exists. No insert performed.")
