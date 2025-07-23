d1e3998c13a31cd1e459c6798c193d2fcf1818db
Mharfe23 <fengiro.arafa@gmail.com>
2025-07-23 21:47:48 +0100

    Add embedding functionality and similarity calculation using Google GenAI

---
+++ embeddings/embedding.py
@@ -0,0 +1,73 @@
+from google import genai
+from google.genai import types
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = genai.Client()
+texts = [
+    "nlp",
+    "natural language processing",
+    "llm",
+    "large language model"
+    ]
+
+result = [
+    np.array(e.values) for e in client.models.embed_content(
+        model="gemini-embedding-001",
+        contents=texts, 
+        config=types.EmbedContentConfig(task_type="SEMANTIC_SIMILARITY")).embeddings
+]
+
+# Calculate cosine similarity. Higher scores = greater semantic similarity.
+
+embeddings_matrix = np.array(result)
+similarity_matrix = cosine_similarity(embeddings_matrix)
+
+print(similarity_matrix)
+
+
+
+
+
+
+
+
+
+
+
+
+
+# from sentence_transformers import SentenceTransformer
+
+# # Load the model
+# model = SentenceTransformer("Qwen/Qwen3-Embedding-4B")
+
+# queries = [
+#     "cypress",
+#     "python",
+#     "testing",
+#     "automated test"
+# ]
+# documents = [
+#     "flask",
+#     "AI",
+#     "selenium",
+    
+# ]
+
+# # Encode the queries and documents. Note that queries benefit from using a prompt
+# # Here we use the prompt called "query" stored under `model.prompts`, but you can
+# # also pass your own prompt via the `prompt` argument
+# query_embeddings = model.encode(queries, prompt_name="query")
+# document_embeddings = model.encode(documents)
+
+# # Compute the (cosine) similarity between the query and document embeddings
+# similarity = model.similarity(query_embeddings, document_embeddings)
+# print(similarity)
+
+# def filter_skills(raw_skills: set, dictionnaire: set):
+#     dict_list = list(dictionnaire)
+    
---
+++ services/dictionaire_service.py
@@ -0,0 +1,2 @@
+from clients.mongo_client import mongo_candidat_init
+
